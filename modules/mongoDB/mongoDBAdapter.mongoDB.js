a2a62e06630e89f370bc4dfea6518055674d411d
Nikhil Muddamsetty <nikhil.muddamsetty@gmail.com>
2022-10-11 08:37:45 +0530

    ADD more functions to mongoDBAdapter
    Added updateMany function
    Added updateOne function
    Added deleteOne function
    Added deleteMany function

--- modules/mongoDB/mongoDBAdapter.mongoDB.js
+++ modules/mongoDB/mongoDBAdapter.mongoDB.js
@@ -185,6 +185,126 @@ class MongoDbAdapter {
     }
   }
 
+  async updateOne(filter = {}, updateFilter = {}, options = {}) {
+    try {
+      let updateOneData = await this.collection.updateOne(
+        filter,
+        updateFilter,
+        options
+      );
+      if (updateOneData.acknowledged) {
+        return response.success("updateOne successful", updateOneData, 200);
+      } else {
+        return response.success(
+          "updateOne failed",
+          updateOneData,
+          500,
+          "DB-UO-1"
+        );
+      }
+    } catch (error) {
+      if (error.code === 11000) {
+        return response.error(
+          `updateOne failed because the field ${
+            Object.keys(error.keyValue)[0]
+          } must be unique`,
+          error.keyValue,
+          400,
+          "DB-UO-DUPLICATE-FIELD"
+        );
+      }
+      return response.error("updateOne failed", error, 500, "UNCAUGHT-DB-UO");
+    }
+  }
+
+  async updateMany(filter = {}, updateFilter = {}, options = {}) {
+    try {
+      let updateManyData = await this.collection.updateMany(
+        filter,
+        updateFilter,
+        options
+      );
+      if (updateManyData.acknowledged) {
+        return response.success("updateMany successful", updateManyData, 200);
+      } else {
+        return response.success(
+          "updateMany failed",
+          updateManyData,
+          500,
+          "DB-UM-1"
+        );
+      }
+    } catch (error) {
+      if (error.code === 11000) {
+        return response.error(
+          `updateMany failed because the field ${
+            Object.keys(error.keyValue)[0]
+          } must be unique`,
+          error.keyValue,
+          400,
+          "DB-UM-DUPLICATE-FIELD"
+        );
+      }
+      return response.error("updateMany failed", error, 500, "UNCAUGHT-DB-UM");
+    }
+  }
+
+  async deleteOne(filter = {}, options = {}) {
+    try {
+      let deleteOneData = await this.collection.deleteOne(filter, options);
+      if (deleteOneData.acknowledged) {
+        return response.success("deleteOne successful", deleteOneData, 200);
+      } else {
+        return response.success(
+          "deleteOne failed",
+          deleteOneData,
+          500,
+          "DB-DO-1"
+        );
+      }
+    } catch (error) {
+      if (error.code === 11000) {
+        return response.error(
+          `deleteOne failed because the field ${
+            Object.keys(error.keyValue)[0]
+          } must be unique`,
+          error.keyValue,
+          400,
+          "DB-DO-DUPLICATE-FIELD"
+        );
+      }
+      return response.error("deleteOne failed", error, 500, "UNCAUGHT-DB-DO");
+    }
+  }
+
+  async deleteMany(filter = {}, options = {}) {
+    try {
+      let deleteManyData = await this.collection.deleteMany(filter, options);
+      if (deleteManyData.acknowledged) {
+        return response.success("deleteMany successful", deleteManyData, 200);
+      } else {
+        return response.success(
+          "deleteMany failed",
+          deleteManyData,
+          500,
+          "DB-DM-1"
+        );
+      }
+    } catch (error) {
+      if (error.code === 11000) {
+        return response.error(
+          `deleteMany failed because the field ${
+            Object.keys(error.keyValue)[0]
+          } must be unique`,
+          error.keyValue,
+          400,
+          "DB-DM-DUPLICATE-FIELD"
+        );
+      }
+      return response.error("deleteMany failed", error, 500, "UNCAUGHT-DB-DM");
+    }
+  }
+
   async pagePropsGenerator(pageSize = undefined, pageNumber = undefined) {
     let limit = 0;
     let skip = 0;
