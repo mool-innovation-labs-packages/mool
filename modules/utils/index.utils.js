4969f3c4bdb520740f9b485ce8f5e3d5fb416a28
Nikhil Muddamsetty <nikhil.muddamsetty@gmail.com>
2022-10-11 08:56:52 +0530

    Exported utils functions

---
+++ modules/utils/index.utils.js
@@ -0,0 +1,9 @@
+const projectionGenerator = require("./projectionGenerator.utils");
+const projectionAndPagePropsGenerator = require("./projectionAndPagePropsGenerator.utils");
+const pagePropsGenerator = require("./pagePropsGenerator.utils");
+
+module.exports = {
+  projectionGenerator,
+  projectionAndPagePropsGenerator,
+  pagePropsGenerator,
+};
